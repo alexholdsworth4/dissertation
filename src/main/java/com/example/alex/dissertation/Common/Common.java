fa42ad46ac585e064b2930f5e2dd58e412e019f3
alexholdsworth4 <47987054+alexholdsworth4@users.noreply.github.com>
2019-02-25 17:32:53 +0000

    Add files via upload

---
+++ src/main/java/com/example/alex/dissertation/Common/Common.java
@@ -0,0 +1,13 @@
+package com.example.alex.dissertation.Common;
+
+import com.example.alex.dissertation.Model.Question;
+import com.example.alex.dissertation.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static String categoryId,categoryName;
+    public static User currentUser;
+    public static List<Question>questionList = new ArrayList<>();
+}
---
+++ src/main/java/com/example/alex/dissertation/ViewHolder/ScoreDetailViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.alex.dissertation.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.alex.dissertation.R;
+
+public class ScoreDetailViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView txt_name,txt_score;
+
+    public ScoreDetailViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txt_name = (TextView)itemView.findViewById(R.id.txt_name);
+        txt_score = (TextView)itemView.findViewById(R.id.txt_score);
+    }
+}
